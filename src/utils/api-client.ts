22f0958c9b9fcd534f2c79d303ad3f7cda520460
Kavya Patel <webdevloper07070@gmail.com>
2025-11-28 09:26:42 +0530

    Update API client to use remote backend URL

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://postman-clone-backend-2ru3.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -44,3 +44,4 @@ export const dataApi = {
 };
 
 export default api;
+
